6f89cf44167f465b21a9325374e87b41a4e292f8
Jijo <jijoraju11@gmail.com>
2022-12-19 17:23:02 -0500

    Added project structure

---
+++ src/main/java/com/jijo/prodstore/ProdStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jijo.prodstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jijo/prodstore/ProdStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jijo.prodstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
